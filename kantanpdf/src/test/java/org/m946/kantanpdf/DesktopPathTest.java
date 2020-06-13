e0bfd671d8c28de0b84b899869bc0f89bfe05398
Jino-946 <xyro@theia.ocn.ne.jp>
2020-06-13 16:10:01 +0900

    インターフェースKantanPDFをクラスに変更する

--- kantanpdf/src/test/java/org/m946/kantanpdf/DesktopPathTest.java
+++ kantanpdf/src/test/java/org/m946/kantanpdf/DesktopPathTest.java
@@ -3,30 +3,30 @@ package org.m946.kantanpdf;
 
 import org.junit.Test;
 
-import com.lowagie.text.PageSize;
+//import org.m946.kantanpdf.PageSize;
 import static org.m946.kantanpdf.DPIUtil.*;
-
 public class DesktopPathTest {
 
 	@Test
 	public void test() {
 		System.out.println(System.getProperty("user.home"));
 	
-		    KantanPDF pdf = new KantanOpenPDF(PageSize.A4);
-		    pdf.newPage()
+		    KantanPDF pdf = new KantanPDF(PageSize.A4);
+		    pdf.enableAutoPageNumber()
+		   .newPage()
 		   .moveTo(cm(1), cm(10))
 		   .lineTo(cm(20), cm(10))
-		   .setTextAlign(KantanPDF.TEXTPOS_LL)
-		   .textOut(cm(5), cm(10), "TEXTPOS_LL")
-		   .setTextAlign(KantanPDF.TEXTPOS_UL)
-		   .textOut(cm(5), cm(10),"TEXTPOS_UL")
-		   .setTextAlign(KantanPDF.TEXTPOS_ML)
-		   .textOut(cm(12), cm(10), "真ん中 左揃え")
-		   .setTextAlign(KantanPDF.TEXTPOS_MR)
-		   .textOut(cm(12), cm(10), "真ん中 右揃え");
+		   .setTextAlign(TextAlign.LowerLeft)
+		   .textOut(cm(5), cm(10), "下ベース 左揃え")
+		   .setTextAlign(TextAlign.UpperLeft)
+		   .textOut(cm(5), cm(10),"上ベース 左揃え")
+		   .setTextAlign(TextAlign.MiddleLeft)
+		   .textOut(cm(12), cm(10), "中ベース 左揃え")
+		   .setTextAlign(TextAlign.MiddleRight)
+		   .textOut(cm(12), cm(10), "中ベース 右揃え")
+		   .newPage()
+		   .newPage();
 		    pdf.saveTo("textalign.pdf");
-		
-		
 	}
 
 }
