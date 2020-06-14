effb1588c124b7fc5fba5b165854a599c1bd8af9
Jino-946 <xyro@theia.ocn.ne.jp>
2020-06-14 22:55:53 +0900

    slf4jロガー追加

--- kantanpdf/src/main/java/org/m946/kantanpdf/samples/Samples.java
+++ kantanpdf/src/main/java/org/m946/kantanpdf/samples/Samples.java
@@ -1,11 +1,14 @@
 package org.m946.kantanpdf.samples;
 import org.junit.Test;
+
+
 import org.m946.kantanpdf.KantanPDF;
 import org.m946.kantanpdf.PageSize;
 import org.m946.kantanpdf.TextAlign;
 
 import static org.m946.kantanpdf.DPIUtil.*;
 
+@lombok.extern.slf4j.Slf4j
 public class Samples {
 	@Test
 	public void helloWorld() {
@@ -17,11 +20,13 @@ public class Samples {
 	// A4サイズ 210 x 297mm
 	@Test
 	public void helloPDF() {
+		log.debug("there.");
 		KantanPDF pdf = new KantanPDF(PageSize.A4.rotate());
+		log.trace("here!!");
 		pdf.setTextAlign(TextAlign.LowerCenter)
 			.setFontSize(30)
 			.textOut(cm(29.7f / 2f), cm(21f / 2f), "Hello, PDF");
-
+		log.info("Hi");
 		pdf.saveTo("samples/hellopdf.pdf");
 	}
 }
